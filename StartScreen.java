2367a74abb2d9cc0dc1534eadec03d31eaac581d
Baxxy <Richard.Otroshchenko@ucdconnect.ie>
2018-04-08 21:04:20 +0100

    Added a startscreen and changed log to include non answers

--- StartScreen.java
+++ StartScreen.java
@@ -1,21 +1,59 @@
-import javax.imageio.ImageIO;
+import java.awt.event.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
-public class StartScreen  extends Component{
-    private BufferedImage image;
+public class StartScreen extends JLabel implements ActionListener
+{
+    JLabel screen;
+    JFrame frame;
+    JButton play,quit;
+    boolean letsPlay = false;
+    public void gameStart()
+    {
+        setLayout(new BorderLayout());
 
-    public StartScreen(){
+        InputStream stream = getClass().getResourceAsStream("CluedoStart.jpg");
+        ImageIcon icon = null;
         try {
-            image = ImageIO.read(new File("CluedoStart.jpg"));
+            icon = new ImageIcon(ImageIO.read(stream));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Couldn't find image...." + e);
         }
+        screen = new JLabel(icon);
+        screen.setVisible(true);
+        frame = new JFrame("Cluedo");
+        play=new JButton("PLAY");
+        quit= new JButton("QUIT");
+
+        frame.add(play, BorderLayout.PAGE_START);
+        frame.add(quit, BorderLayout.PAGE_END);
+        frame.add(screen, BorderLayout.CENTER);
+
+        play.addActionListener(this);
+        quit.addActionListener(this);
+
+        frame.setSize(600,370);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
     }
 
-    public void paint(Graphics g){
-        g.drawImage(image, 0, 0, null);
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==play){
+            letsPlay = true;
+            frame.dispose();
+        }
+
+        if(e.getSource()==quit)
+        {
+            System.exit(0);
+        }
+
+
     }
+
 }
