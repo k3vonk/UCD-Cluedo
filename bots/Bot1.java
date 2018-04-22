1e50bad902bb515193f85f311389f3946e61f41c
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2018-04-22 19:01:53 +0100

    A*

--- bots/Bot1.java
+++ bots/Bot1.java
@@ -1,5 +1,12 @@
 package bots;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 import gameengine.*;
 
 public class Bot1 implements BotAPI {
@@ -17,8 +24,14 @@ public class Bot1 implements BotAPI {
     private Log log;
     private Deck deck;
     private int diceTotal;
+    private int squaresMoved;
+    private int pathLeft;
+    Random rand = new Random();
+    String mapDirections[] = {"u", "d", "l", "r"};
+    ArrayList<Coordinates> path;
 
-    public Bot1 (Player player, PlayersInfo playersInfo, Map map, Dice dice, Log log, Deck deck) {
+
+    public Bot1(Player player, PlayersInfo playersInfo, Map map, Dice dice, Log log, Deck deck) {
         this.player = player;
         this.playersInfo = playersInfo;
         this.map = map;
@@ -32,13 +45,13 @@ public class Bot1 implements BotAPI {
         return "MAGA"; // must match the class name
     }
 
-    public String getVersion () {
+    public String getVersion() {
         return "0.1";   // change on a new release
     }
 
     public String getCommand() {
         //Roll dice
-    	if(diceTotal == -1) {
+    	/*if(diceTotal == -1) {
     		return "roll";
     	}//In corridor and has moves
     	else if(map.isCorridor(player.getToken().getPosition()) && diceTotal > 0) {
@@ -59,12 +72,67 @@ public class Bot1 implements BotAPI {
     	
     	//Ending
     	diceTotal = -1;
-        return "done";
+        return "done";*/
+
+        if (squaresMoved == dice.getTotal()) {
+            squaresMoved = 0;
+            return "done";
+        }else if (map.isCorridor(player.getToken().getPosition())) {
+            return "roll";
+        } else if (!map.isCorridor(player.getToken().getPosition()) && dice.getTotal() > 0) {
+            //exit room or
+            //passage
+            return "done";
+        } else if (!map.isCorridor(player.getToken().getPosition()) && diceTotal == 0) {
+            //Accusation room
+            //Accuse
+            return "done";
+        } else {
+            return "help";
+        }
+
     }
 
     public String getMove() {
-       
-        return "r";
+
+        if (pathLeft == 0) {
+            path = calculatePath(player.getToken().getPosition(),
+                    map.getRoom("Conservatory").getDoorCoordinates(0));
+            pathLeft += path.size();
+        }
+
+        String randMove = getDirection(player.getToken().getPosition(),
+                path.remove(path.size() - 1));
+        System.out.println("Direction:" + randMove);
+        pathLeft--;
+        squaresMoved += 1;
+        return randMove;
+
+    }
+
+
+    private ArrayList<Coordinates> calculatePath(Coordinates s, Coordinates e) {
+
+        BZAstar pathFinder = new BZAstar(24, 25);
+        ArrayList<Coordinates> path = pathFinder.calculateAStarNoTerrain(
+                player.getToken().getPosition(), map.getRoom("Conservatory").getDoorCoordinates(0));
+        return path;
+    }
+
+    private String getDirection(Coordinates start, Coordinates end) {
+
+        System.out.println("Moving from: " + start + " to: " + end);
+        if (start.getRow() < end.getRow()) {
+            return "d";
+        } else if (start.getRow() > end.getRow()) {
+            return "u";
+        } else if (start.getCol() > end.getCol()) {
+            return "l";
+        } else if (start.getCol() < end.getCol()) {
+            return "r";
+        }
+
+        return null;
     }
 
     public String getSuspect() {
@@ -112,4 +180,148 @@ public class Bot1 implements BotAPI {
         // Add your code here
     }
 
+
+    class BZAstar {
+
+        private final int width;
+        private final int height;
+
+        private final HashMap<String, AStarNode> nodes = new HashMap<>();
+
+        @SuppressWarnings("rawtypes")
+        private final Comparator fComparator = new Comparator<AStarNode>() {
+            public int compare(AStarNode a, AStarNode b) {
+                return Integer.compare(a.getFValue(), b.getFValue()); //ascending to get the lowest
+            }
+        };
+
+        public BZAstar(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    String pointS = "(" + x + "," + y + ")";
+                    Coordinates point = new Coordinates(x, y);
+                    this.nodes.put(pointS, new AStarNode(point));
+                }
+            }
+
+
+        }
+
+        @SuppressWarnings("unchecked")
+        public ArrayList<Coordinates> calculateAStarNoTerrain(Coordinates p1, Coordinates p2) {
+
+            List<AStarNode> openList = new ArrayList<AStarNode>();
+            List<AStarNode> closedList = new ArrayList<AStarNode>();
+
+            AStarNode currentNode, destNode;
+            destNode = nodes.get("(" + p2.getCol() + "," + p2.getRow() + ")");
+            currentNode = nodes.get("(" + p1.getCol() + "," + p1.getRow() + ")");
+            currentNode.parent = null;
+            currentNode.setGValue(0);
+            openList.add(currentNode);
+
+
+            while (!openList.isEmpty()) {
+
+                Collections.sort(openList, this.fComparator);
+                currentNode = openList.get(0);
+
+                if (currentNode.point.equals(destNode.point)) {
+                    return this.calculatePath(destNode);
+                }
+
+                openList.remove(currentNode);
+                closedList.add(currentNode);
+
+                for (String direction : mapDirections) {
+                    Coordinates adjPoint = map.getNewPosition(currentNode.point, direction);
+                    if (!this.isInsideBounds(adjPoint)) {
+                        continue;
+                    }
+                    AStarNode adjNode = nodes.get(
+                            "(" + adjPoint.getCol() + "," + adjPoint.getRow() + ")");
+                    if (!map.isValidMove(currentNode.point, direction)) {
+                        continue;
+                    }
+
+                    if (!closedList.contains(adjNode)) {
+                        if (!openList.contains(adjNode)) {
+                            adjNode.parent = currentNode;
+                            adjNode.calculateGValue(currentNode);
+                            adjNode.calculateHValue(destNode);
+                            openList.add(adjNode);
+                        } else {
+                            if (adjNode.gValue < currentNode.gValue) {
+                                adjNode.calculateGValue(currentNode);
+                                currentNode = adjNode;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private ArrayList<Coordinates> calculatePath(AStarNode destinationNode) {
+            ArrayList<Coordinates> path = new ArrayList<Coordinates>();
+            AStarNode node = destinationNode;
+            while (node.parent != null) {
+                path.add(node.point);
+                node = node.parent;
+            }
+            return path;
+        }
+
+        private boolean isInsideBounds(Coordinates point) {
+            return point.getCol() >= 0 &&
+                    point.getCol() < this.width &&
+                    point.getRow() >= 0 &&
+                    point.getRow() < this.height;
+        }
+
+
+    }
+
+
+    class AStarNode {
+
+        public final Coordinates point;
+
+        public AStarNode parent;
+
+        public int gValue; //points from start
+        public int hValue; //distance from target
+
+        private final int MOVEMENT_COST = 10;
+
+        public AStarNode(Coordinates point) {
+            this.point = point;
+        }
+
+        /**
+         * Used for setting the starting node value to 0
+         */
+        public void setGValue(int amount) {
+            this.gValue = amount;
+        }
+
+        public void calculateHValue(AStarNode destPoint) {
+            this.hValue = (Math.abs(point.getCol() - destPoint.point.getCol()) + Math.abs(
+                    point.getRow() - destPoint.point.getRow())) * this.MOVEMENT_COST;
+        }
+
+        public void calculateGValue(AStarNode point) {
+            this.gValue = point.gValue + this.MOVEMENT_COST;
+        }
+
+        public int getFValue() {
+            return this.gValue + this.hValue;
+        }
+    }
+
 }
+
